24b08621b5aeffdbb8b624b44f9f082862d651fa
Rmacias91 <Rmacias2454@gmail.com>
2019-12-14 13:06:15 -0500

    Added Base Fragment and View Model

---
+++ PiConnect/app/src/main/java/com/pi/connect/ConnectFragment.kt
@@ -0,0 +1,28 @@
+package com.pi.connect
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun createConnectFragment() = ConnectFragment()
+class ConnectFragment : Fragment() {
+
+    private lateinit var viewModel: ConnectViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.connect_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ConnectViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
--- PiConnect/app/src/main/java/com/pi/connect/MainActivity.kt
+++ PiConnect/app/src/main/java/com/pi/connect/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, createConnectFragment())
+            .commit()
     }
 }

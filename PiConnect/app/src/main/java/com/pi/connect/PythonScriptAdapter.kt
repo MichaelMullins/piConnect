a34ab62a09d01beadb6135597893d17e161878c2
michaelMullins <michaelmullins372@gmail.com>
2019-12-17 10:38:07 -0500

    Added RecyclerView

---
+++ PiConnect/app/src/main/java/com/pi/connect/PythonScriptAdapter.kt
@@ -0,0 +1,31 @@
+package com.pi.connect
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.python_scripts.view.*
+
+class PythonScriptAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    // Gets the number of animals in the list
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // Inflates the item views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.python_scripts, parent, false))
+    }
+
+    // Binds each animal in the ArrayList to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvPythonScript?.text = items.get(position)
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    // Holds the TextView that will add each animal to
+    val tvPythonScript = view.tv_pythonScript
+}
